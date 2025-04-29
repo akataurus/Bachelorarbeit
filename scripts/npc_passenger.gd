ba6255577782083dec1d424631730627e6a4e89b
David Jenzer <jenzerdavid5@gmail.com>
2025-04-29 14:57:55 +0200

    npc can move along a path

--- scripts/npc_passenger.gd
+++ scripts/npc_passenger.gd
@@ -1,24 +1,25 @@
 extends CharacterBody3D
 
 @export var speed := 2.0
-@export var target_position:= Vector3(5,5,5)
-@export var stop_distance := 0.7 # ab wann er "am Ziel" ist
+@export var target_position: Vector3
+
+var path0 := [Vector3(10, 0, 0), Vector3(0, 5, 0), Vector3(5, 0, 0)]
+var path_index = 0
 
 func _physics_process(delta: float) -> void:
-	var direction = (target_position - global_transform.origin).normalized()
+	if path_index >= path0.size():
+		queue_free()
+		return
+	
+	var direction = (path0[path_index] - global_transform.origin).normalized()
 	velocity = direction * speed
 	move_and_slide()
 	
-	if global_transform.origin.distance_to(target_position) < 0.1:
+	if global_transform.origin.distance_to(path0[path_index]) < 0.1:
 		print("angekommen")
-		queue_free()
-	#if direction.length() > stop_distance:
-		#direction = direction.normalized()
-		#velocity = direction * speed
-		#move_and_slide()
-	#else:
-		#print("queue free")
-		#queue_free() # npc verschwind
+		path_index = path_index +1
+		#queue_free() # npc verschwindet
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
