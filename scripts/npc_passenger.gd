53bce680871ce7bd8dfa9155b51f277fd4896c9b
David Jenzer <jenzerdavid5@gmail.com>
2025-04-29 14:27:44 +0200

    npcs despawnen

--- scripts/npc_passenger.gd
+++ scripts/npc_passenger.gd
@@ -2,11 +2,23 @@ extends CharacterBody3D
 
 @export var speed := 2.0
 @export var target_position:= Vector3(5,5,5)
+@export var stop_distance := 0.7 # ab wann er "am Ziel" ist
 
 func _physics_process(delta: float) -> void:
 	var direction = (target_position - global_transform.origin).normalized()
 	velocity = direction * speed
 	move_and_slide()
+	
+	if global_transform.origin.distance_to(target_position) < 0.1:
+		print("angekommen")
+		queue_free()
+	#if direction.length() > stop_distance:
+		#direction = direction.normalized()
+		#velocity = direction * speed
+		#move_and_slide()
+	#else:
+		#print("queue free")
+		#queue_free() # npc verschwind
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
