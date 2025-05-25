114fb78876a2c1735efc0187059a6243a5bb976a
David Jenzer <jenzerdavid5@gmail.com>
2025-05-25 14:22:21 +0200

    gate counter

---
+++ scenes/gate_counter.gd
@@ -0,0 +1,39 @@
+extends Node3D
+
+@onready var airline_worker := $airline_worker
+@onready var speech_bubble := $"airline_worker".get_node("speech_bubble")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if GameManager.role != "passenger":
+		airline_worker.visible = false
+	
+	speech_bubble.text = "Welcome to the gate! \n Please show your boarding ticket."
+	speech_bubble.visible = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("show_bcard"):
+		boarding_card_dialogue()
+
+
+
+func boarding_card_dialogue():
+	if !GameManager.is_checked_in:
+		speech_bubble.text = "Oh no, you are not checked in yet! \n Please go to the counter and check in."
+		await get_tree().create_timer(4).timeout  # Delay in Sekunden
+		speech_bubble.text = ""
+	if GameManager.is_checked_in and !GameManager.is_sec_checked:
+		speech_bubble.text = "Oh no, you weren't at the security check! \n Please go there first."
+		await get_tree().create_timer(4).timeout  # Delay in Sekunden
+		speech_bubble.text = ""
+	if GameManager.is_checked_in and GameManager.is_sec_checked:
+		speech_bubble.text = "Thanks! You can go on board now."
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		speech_bubble.visible = true
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		speech_bubble.visible = false
--- scripts/gameManager.gd
+++ scripts/gameManager.gd
@@ -1,7 +1,8 @@
 extends Node
 
 var role := "" # can be "passenger", "airport_worker" or "airline_worker"
-var is_checked_in := false # will be true after passenger is checked in
+var is_checked_in := false # true after passenger is checked in
+var is_sec_checked := false # true after security check
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
